3f51f231d710130b8f31c9edeb71303a422f83fd
hassan-jamshaid10 <hjamshaid81@gmail.com>
2024-10-16 22:27:53 +0500

    made changes in header

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,4 +1,3 @@
-// src/components/Header.js
 import React, { useState, useCallback } from 'react';
 
 const Header = () => {
@@ -13,18 +12,20 @@ const Header = () => {
     <header className="bg-black fixed w-full z-10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <div className="flex items-center">
-            
+          <div className="flex items-center justify-center w-full">
             <div className="hidden md:block">
-              <div className="ml-10 flex items-baseline space-x-4">
-                <a href="#home" className="text-gray-300 hover:text-[#229799] px-3 py-2 rounded-md text-sm font-medium">Home</a>
-                <a href="#about" className="text-gray-300 hover:text-[#229799] px-3 py-2 rounded-md text-sm font-medium">About</a>
-                <a href="#projects" className="text-gray-300 hover:text-[#229799] px-3 py-2 rounded-md text-sm font-medium">Projects</a>
-                <a href="#services" className="text-gray-300 hover:text-[#229799] px-3 py-2 rounded-md text-sm font-medium">Services</a>
-                <a href="#contact" className="text-gray-300 hover:text-[#229799] px-3 py-2 rounded-md text-sm font-medium">Contact</a>
+              <div className="ml-10 flex justify-center items-center space-x-10 text-white">
+                
+                <a href="#home" className="text-white hover:text-[#229799] px-3 py-2 rounded-md text-sm font-medium">Home</a>
+                <a href="#about" className="text-white hover:text-[#229799] px-3 py-2 rounded-md text-sm font-medium">About</a>
+                <a href="#projects" className="text-white hover:text-[#229799] px-3 py-2 rounded-md text-sm font-medium">Projects</a>
+                <a href="#services" className="text-white hover:text-[#229799] px-3 py-2 rounded-md text-sm font-medium">Services</a>
+                <a href="#contact" className="text-white hover:text-[#229799] px-3 py-2 rounded-md text-sm font-medium">Contact</a>
+          
               </div>
             </div>
           </div>
+
           <div className="-mr-2 flex md:hidden">
             <button onClick={toggleMenu} className="bg-black inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white">
               <span className="sr-only">Open main menu</span>
